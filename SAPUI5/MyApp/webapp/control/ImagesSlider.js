e426096ea31597a946cb2dc797fd923078bd7b76
Chebykin Ivan <chebykin.i.v@gmail.com>
2019-04-12 19:29:09 +0500

    Added empty custom control and model to it

---
+++ SAPUI5/MyApp/webapp/control/ImagesSlider.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/core/Control"
+], function (Control) {
+	"use strict";
+	return Control.extend("opensap.myapp.control.ImagesSlider", {
+		metadata : {
+		},
+		init : function () {
+		},
+		renderer : function (oRM, oControl) {
+			
+		}
+	});
+});
