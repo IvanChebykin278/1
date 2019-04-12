60ce8ba11c92b5af919c1de1909896b879a309f3
Chebykin Ivan <chebykin.i.v@gmail.com>
2019-04-12 19:38:57 +0500

    added properties to ImagesSlider

--- SAPUI5/MyApp/webapp/control/ImagesSlider.js
+++ SAPUI5/MyApp/webapp/control/ImagesSlider.js
@@ -4,8 +4,15 @@ sap.ui.define([
 	"use strict";
 	return Control.extend("opensap.myapp.control.ImagesSlider", {
 		metadata : {
+			properties: {
+				images: {type: "object[]", defaultValue: [{}]},
+				width: {type: "sap.ui.core.CSSSize", defaultValue: "100%"},
+				height: {type: "sap.ui.core.CSSSize", defaultValue: "auto"},
+				slideScrollingTime: {type: "float", defaultValue: 5}
+			}
 		},
 		init : function () {
+			
 		},
 		renderer : function (oRM, oControl) {
 			
